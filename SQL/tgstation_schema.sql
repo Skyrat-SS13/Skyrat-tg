67137b906dde144d8be6cf49b84fccdabbd192c0
MissFox <github@aesir.org.uk>
2021-01-12 18:36:32 +0100

    I think i fixed the sql....

--- SQL/tgstation_schema.sql
+++ SQL/tgstation_schema.sql
@@ -592,9 +592,9 @@ CREATE TABLE `discord_links` (
 	PRIMARY KEY (`id`)
 ) ENGINE=InnoDB;
 
----
----Table structure for table `game_log`
----
+--
+-- Table structure for table `game_log`
+--
 DROP TABLE IF EXISTS `game_log`;
 CREATE TABLE `game_log` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
