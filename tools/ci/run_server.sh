ac2192b6993bcfd3b4bc6d2e32050942659b68c9
Gandalf2k15 <jzo123@hotmail.com>
2021-02-03 14:43:22 +0000

    a

--- tools/ci/run_server.sh
+++ tools/ci/run_server.sh
@@ -10,6 +10,6 @@ mv ci_test/config/config.txt ci_test/config/original_config.txt
 cp tools/ci/ci_config.txt ci_test/config/config.txt
 
 cd ci_test
-DreamDaemon tgstation.dmb -close -trusted -verbose -params "log-directory=ci&original_config=original_config.txt"
+DreamDaemon tgstation.dmb -close -trusted -verbose -params "log-directory=ci"
 cd ..
 cat ci_test/data/logs/ci/clean_run.lk
