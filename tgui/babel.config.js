ff2bb52ec4d5b0d4e0851f8023edc1739cca6775
Gandalf <jzo123@hotmail.com>
2021-02-28 14:55:32 +0000

    Update babel.config.js

--- tgui/babel.config.js
+++ tgui/babel.config.js
@@ -8,14 +8,10 @@ const createBabelConfig = options => {
   const { mode, presets = [], plugins = [] } = options;
   return {
     presets: [
-<<<<<<< HEAD
-      ['@babel/preset-env', {
-=======
       [require.resolve('@babel/preset-typescript'), {
         allowDeclareFields: true,
       }],
       [require.resolve('@babel/preset-env'), {
->>>>>>> 21d5a65346c (Fix a number of build issues (#57251))
         modules: 'commonjs',
         useBuiltIns: 'entry',
         corejs: '3.8',
@@ -26,12 +22,6 @@ const createBabelConfig = options => {
       ...presets,
     ],
     plugins: [
-<<<<<<< HEAD
-      '@babel/plugin-transform-jscript',
-      'babel-plugin-inferno',
-      'babel-plugin-transform-remove-console',
-      'common/string.babel-plugin.cjs',
-=======
       [require.resolve('@babel/plugin-proposal-class-properties'), {
         loose: true,
       }],
@@ -39,13 +29,12 @@ const createBabelConfig = options => {
       require.resolve('babel-plugin-inferno'),
       require.resolve('babel-plugin-transform-remove-console'),
       require.resolve('common/string.babel-plugin.cjs'),
->>>>>>> 21d5a65346c (Fix a number of build issues (#57251))
       ...plugins,
     ],
   };
 };
 
-module.exports = (api) => {
+module.exports = api => {
   api.cache(true);
   const mode = process.env.NODE_ENV;
   return createBabelConfig({ mode });
