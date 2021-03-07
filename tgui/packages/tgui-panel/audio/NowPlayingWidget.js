9482521db9d6f7ec2cb28c5e67e18f760f30fb3d
Gandalf2k15 <jzo123@hotmail.com>
2021-03-07 00:34:10 +0000

    a

--- tgui/packages/tgui-panel/audio/NowPlayingWidget.js
+++ tgui/packages/tgui-panel/audio/NowPlayingWidget.js
@@ -33,7 +33,7 @@ export const NowPlayingWidget = (props, context) => {
               'overflow': 'hidden',
               'text-overflow': 'ellipsis',
             }}>
-            {'An admin-played soundtrack.'}
+            {title || 'Unknown Track'}
           </Flex.Item>
         </>
       ) || (
