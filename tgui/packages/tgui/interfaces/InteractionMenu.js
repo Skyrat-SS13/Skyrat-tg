3a212d86097acf55d5591a28374be94cde3ce1a5
Matthew <matthew@tfaluc.com>
2021-05-27 02:25:56 -0400

    linters

--- tgui/packages/tgui/interfaces/InteractionMenu.js
+++ tgui/packages/tgui/interfaces/InteractionMenu.js
@@ -25,7 +25,7 @@ export const InteractionMenu = (props, context) => {
               content={int}
               color={block_interact ? "grey" : "blue"}
               onClick={() => act('interact', { interaction: int, selfref: ref_self, userref: ref_user })}
-                />
+            />
           ))}
         </Section>
       ))}
