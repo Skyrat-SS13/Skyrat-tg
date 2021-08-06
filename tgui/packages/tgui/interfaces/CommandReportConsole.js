12abdaec8fdf1e1436938a0bc23051df93a3e266
Gandalf <jzo123@hotmail.com>
2021-08-06 02:34:52 +0100

    A

---
+++ tgui/packages/tgui/interfaces/CommandReportConsole.js
@@ -0,0 +1,53 @@
+import { useBackend } from '../backend';
+import { Button, Dropdown, Input, Section, Stack, TextArea } from '../components';
+import { Window } from '../layouts';
+
+export const CommandReport = (props, context) => {
+  const { act, data } = useBackend(context);
+  const {
+    command_report_content,
+    played_sound,
+    announce_contents,
+  } = data;
+  return (
+    <Window
+      title="Create Fleet Report"
+      width={325}
+      height={525}>
+      <Window.Content>
+        <Stack vertical>
+          <Stack.Item>
+            <Section title="Set report text:" textAlign="center">
+              <TextArea
+                height="200px"
+                mb={1}
+                value={command_report_content}
+                onChange={(e, value) => act("update_report_contents", {
+                  updated_contents: value,
+                })} />
+              <Stack vertical>
+                <Stack.Item>
+                  <Button.Checkbox
+                    fluid
+                    checked={announce_contents}
+                    onClick={() => act("toggle_announce")}>
+                    Announce Contents
+                  </Button.Checkbox>
+                </Stack.Item>
+                <Stack.Item>
+                  <Button.Confirm
+                    fluid
+                    icon="check"
+                    color="good"
+                    textAlign="center"
+                    content="Submit Report"
+                    onClick={() => act("submit_report")} />
+                </Stack.Item>
+              </Stack>
+            </Section>
+          </Stack.Item>
+        </Stack>
+      </Window.Content>
+    </Window>
+  );
+};
