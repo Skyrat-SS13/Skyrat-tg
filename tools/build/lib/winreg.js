cae83965384c770c65605dedba1f76fcee551a47
SkyratBot <59378654+SkyratBot@users.noreply.github.com>
2021-08-17 20:15:40 +0200

    [MIRROR] tgui maintenance chores (#7597)
    * tgui maintenance chores
    
    * AAAAAA
    
    Co-authored-by: Aleksej Komarov <stylemistake@gmail.com>
    Co-authored-by: Gandalf <jzo123@hotmail.com>

--- tools/build/lib/winreg.js
+++ tools/build/lib/winreg.js
@@ -1,16 +1,15 @@
 /**
  * Tools for dealing with Windows Registry bullshit.
  *
+ * Adapted from `tgui/packages/tgui-dev-server/winreg.js`.
+ *
  * @file
- * @copyright 2020 Aleksej Komarov
+ * @copyright 2021 Aleksej Komarov
  * @license MIT
  */
 
 import { exec } from 'child_process';
 import { promisify } from 'util';
-import { createLogger } from './logging.js';
-
-const logger = createLogger('winreg');
 
 export const regQuery = async (path, key) => {
   if (process.platform !== 'win32') {
@@ -22,26 +21,22 @@ export const regQuery = async (path, key) => {
     const keyPattern = `    ${key}    `;
     const indexOfKey = stdout.indexOf(keyPattern);
     if (indexOfKey === -1) {
-      logger.error('could not find the registry key');
       return null;
     }
     const indexOfEol = stdout.indexOf('\r\n', indexOfKey);
     if (indexOfEol === -1) {
-      logger.error('could not find the end of the line');
       return null;
     }
     const indexOfValue = stdout.indexOf(
       '    ',
       indexOfKey + keyPattern.length);
     if (indexOfValue === -1) {
-      logger.error('could not find the start of the key value');
       return null;
     }
     const value = stdout.substring(indexOfValue + 4, indexOfEol);
     return value;
   }
   catch (err) {
-    logger.error(err);
     return null;
   }
 };
