fc06e5aeda34e6644d05877ec4d83888a41c0ea4
Gandalf <jzo123@hotmail.com>
2021-09-24 22:43:09 +0100

    Fix donator items spamming exploit checks

--- tgui/packages/tgui/interfaces/LoadoutManager.js
+++ tgui/packages/tgui/interfaces/LoadoutManager.js
@@ -105,7 +105,6 @@ export const LoadoutManager = (props, context) => {
                                 <Button
                                   icon="lock"
                                   onClick={() => act('display_restrictions', {
-                                    path: item.path,
                                   })} />
                               </Stack.Item>
                             )}
@@ -116,7 +115,6 @@ export const LoadoutManager = (props, context) => {
                                   icon="heart"
                                   color="pink"
                                   onClick={() => act('donator_explain', {
-                                    path: item.path,
                                   })}
                                 />
                               </Stack.Item>
