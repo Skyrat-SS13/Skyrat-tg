596dc7116f68bf1e5c53808ca55732e4c15b7995
Azarak <azarak10@gmail.com>
2021-12-07 00:19:45 +0100

    wow what a linter

--- tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/game_preferences/skyrat/combat_mode_sticky_directions.tsx
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/game_preferences/skyrat/combat_mode_sticky_directions.tsx
@@ -5,7 +5,8 @@ export const combat_mode_sticky_directions: FeatureToggle = {
   name: "Enable Combat Mode Sticky Directions",
   category: "GAMEPLAY",
   description: multiline`
-    When toggled, your movements will no longer turn your character after facing something for a moment.
+    When toggled, your movements will no longer 
+    turn your character after facing something for a moment.
   `,
   component: CheckboxInput,
 };
