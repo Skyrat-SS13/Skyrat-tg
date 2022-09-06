dc7c109e877bef229d0774d779eb0f9f034cfd53
RimiNosha <riminosha@gmail.com>
2022-09-06 23:46:34 +0100

    Make the option be more obvious in what it does!

--- tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/skyrat/brain_type.tsx
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/skyrat/brain_type.tsx
@@ -1,6 +1,6 @@
 import { FeatureChoiced, FeatureDropdownInput } from '../../base';
 
 export const brain_type: FeatureChoiced = {
-  name: 'Brain Type',
+  name: 'Silicon and Synthetic Brain Type',
   component: FeatureDropdownInput,
 };
