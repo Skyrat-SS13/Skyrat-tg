5d21fc510c82813247d3bdfd3832a20865c586b3
John Doe <gamingskeleton3@gmail.com>
2022-10-08 08:55:33 -0700

    _

--- tgui/packages/tgui/interfaces/BorerEvolution.tsx
+++ tgui/packages/tgui/interfaces/BorerEvolution.tsx
@@ -95,7 +95,7 @@ const EvolutionList = (props, context) => {
                 tooltip={
                   toLearn.exclusive
                     ? toLearn.desc +
-                    `By taking this, you cannot take other T3+ gneomes.`
+                    ` By taking this, you cannot take other T3+ gneomes.`
                     : toLearn.desc
                 }
                 onClick={() => act('evolve', { path: toLearn.path })}
