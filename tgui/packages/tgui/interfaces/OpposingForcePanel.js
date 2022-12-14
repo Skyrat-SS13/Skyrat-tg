a6e83844b12e833c1b230940f3e8101946efdfe1
Zonespace <41448081+Zonespace27@users.noreply.github.com>
2022-12-14 10:20:23 -0800

    Update OpposingForcePanel.js

--- tgui/packages/tgui/interfaces/OpposingForcePanel.js
+++ tgui/packages/tgui/interfaces/OpposingForcePanel.js
@@ -182,7 +182,7 @@ export const OpposingForceTab = (props, context) => {
             disabled={!can_edit}
             height="100px"
             value={backstory}
-            placeholder="Provide a description of why you want to do bad things. Include specifics such as what lead upto the events that made you want to do bad things, think of it as though you were your character, react appropriately. If you don't have any ideas, check the #INSERT_CHANNEL_NAME_HERE channel for some."
+            placeholder="Provide a description of why you want to do bad things. Include specifics such as what lead upto the events that made you want to do bad things, think of it as though you were your character, react appropriately. If you don't have any ideas, check the #player-shared-opfors channel for some."
             onChange={(_e, value) =>
               act('set_backstory', {
                 backstory: value,
