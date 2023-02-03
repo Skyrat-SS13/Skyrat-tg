da90bccff8c0532c16e54bd5cf937c9fa7ee2b72
lessthanthree <83487515+lessthnthree@users.noreply.github.com>
2023-02-03 11:33:21 -0800

    Update tgui/packages/tgui/interfaces/CommunicationsConsole.js
    Co-authored-by: GoldenAlpharex <58045821+GoldenAlpharex@users.noreply.github.com>

--- tgui/packages/tgui/interfaces/CommunicationsConsole.js
+++ tgui/packages/tgui/interfaces/CommunicationsConsole.js
@@ -384,7 +384,7 @@ const PageMain = (props, context) => {
           )}
           {!!canToggleEmergencyAccess && (
             <Button.Confirm
-              icon="wrench" // SKYRAT EDIT
+              icon="id-card-o"
               content={`${
                 emergencyAccess ? 'Disable' : 'Enable'
               } Emergency Maintenance Access`}
