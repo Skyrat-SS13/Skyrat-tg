97c8713bcc83f2965f88cb91be24d24cafc739f5
Antonio Tosti <5588048+atosti@users.noreply.github.com>
2023-03-13 18:21:55 -0700

    Re-implements Physical and Mental statuses in crewmember Medical Records

--- tgui/packages/tgui/interfaces/MedicalRecords/types.ts
+++ tgui/packages/tgui/interfaces/MedicalRecords/types.ts
@@ -3,6 +3,12 @@ import { BooleanLike } from 'common/react';
 export type MedicalRecordData = {
   assigned_view: string;
   authenticated: BooleanLike;
+<<<<<<< HEAD
+=======
+  station_z: BooleanLike;
+  physical_statuses: string[];
+  mental_statuses: string[];
+>>>>>>> 73172f88365 (Re-implements Physical and Mental statuses in crewmember Medical Records (#73882))
   records: MedicalRecord[];
   min_age: number;
   max_age: number;
@@ -16,6 +22,8 @@ export type MedicalRecord = {
   gender: string;
   major_disabilities: string;
   minor_disabilities: string;
+  physical_status: string;
+  mental_status: string;
   name: string;
   notes: MedicalNote[];
   quirk_notes: string;
