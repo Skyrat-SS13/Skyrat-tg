9d5b28b5cb52a8496717ca23f60e2d7ead2dd777
Plum <50682821+Plum-but-gayer@users.noreply.github.com>
2023-10-05 12:53:38 -0400

    changed choiced to toggle
    swapped choiced for toggle also more scrubbing of stuff from the original file i copied, got rid of 'antag interaction preferences' this is just a button that controls if you want to be a mechanical antag target or not now.

--- tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/skyrat/antag_optin.tsx
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/skyrat/antag_optin.tsx
@@ -1,11 +1,6 @@
-import { FeatureChoiced, FeatureDropdownInput } from '../../base';
+import { CheckboxInput, FeatureToggle } from '../../base';
 
-export const antag_opt_in_status_pref: FeatureChoiced = {
+export const antag_opt_in_status_pref: FeatureToggle = {
   name: 'Be Antagonist Target',
-  component: FeatureDropdownInput,
-};
-
-export const antag_opt_in_status_pref_interaction: FeatureChoiced = {
-  name: 'Antagonist Interaction Preference',
-  component: FeatureDropdownInput,
+  component: CheckboxInput,
 };
