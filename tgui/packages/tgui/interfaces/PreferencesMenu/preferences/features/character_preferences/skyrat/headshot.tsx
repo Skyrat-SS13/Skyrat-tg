d39cd02863b806b0813f9673c26195a8850a74fd
nikothedude <simon.prouty@gmail.com>
2023-11-04 12:25:21 -0400

    cick

--- tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/skyrat/headshot.tsx
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/skyrat/headshot.tsx
@@ -3,7 +3,8 @@ import { Feature, FeatureShortTextInput } from '../../base';
 export const headshot: Feature<string> = {
   name: 'Headshot',
   description:
-    'Requires a link ending with .png. Renders the image \
-    underneath your character preview in the examine more window.',
+    'Requires a link ending with .png, .jpeg, or .jpg, and starting with \
+    https://. Renders the image underneath your character preview in the \
+    examine more window.',
   component: FeatureShortTextInput,
 };
