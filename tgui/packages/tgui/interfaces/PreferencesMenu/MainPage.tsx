dda587ab0bcec5e4e8334e8a05f890351ea5f837
nikothedude <59709059+nikothedude@users.noreply.github.com>
2023-11-16 21:02:43 -0500

    Allows preference list features to control the order in which they are sorted (#79579)

--- tgui/packages/tgui/interfaces/PreferencesMenu/MainPage.tsx
+++ tgui/packages/tgui/interfaces/PreferencesMenu/MainPage.tsx
@@ -355,7 +355,11 @@ const createSetRandomization =
 
 const sortPreferences = sortBy<[string, unknown]>(([featureId, _]) => {
   const feature = features[featureId];
-  return feature?.name;
+  if (feature?.sortingPrefix) {
+    return feature.sortingPrefix + feature.name;
+  } else {
+    return feature?.name;
+  }
 });
 
 export const PreferenceList = (props: {
