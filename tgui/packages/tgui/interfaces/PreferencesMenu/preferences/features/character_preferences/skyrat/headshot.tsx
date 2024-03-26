5815f0bb7e8f23141186671cf61ddbfbbe73cb97
YakumoChen <king_yoshi42@yahoo.com>
2024-03-26 06:12:01 +0000

    Update headshot.tsx
    f-list is not a host here

--- tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/skyrat/headshot.tsx
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/skyrat/headshot.tsx
@@ -5,7 +5,7 @@ export const headshot: Feature<string> = {
   name: 'Headshot',
   description:
     'Requires a link ending with .png, .jpeg, or .jpg, starting with \
-    https://, and hosted on Gyazo, Lensdump, F-List, or Imgbox. \
+    https://, and hosted on Gyazo, Lensdump, or Imgbox. \
     Renders the image underneath your character preview in the examine more window. \
     Image larger than 250x250 will be resized to 250x250. \
     Aim for 250x250 whenever possible',
