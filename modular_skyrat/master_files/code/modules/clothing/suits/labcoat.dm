AzriAnDr/HowlingVoid

For Science: Slightly resprites RD labcoat variants and adds regular labcoat to loadout (#1009)
slightly resprites multiple labcoats and adds regular labcoat

modular_nova/master_files/code/modules/clothing/suits/labcoat.dm
@@ -21,6 +21,11 @@
 	fire = 80
 	acid = 70
 
+/obj/item/clothing/suit/toggle/labcoat/nova/regular
+	name = "researcher's labcoat"
+	desc = "A Nanotrasen standard labcoat for researchers in the scientific field."
+	icon_state = "labcoat_regular"
+
 /obj/item/clothing/suit/toggle/labcoat/nova/highvis
 	name = "high vis labcoat"
 	desc = "A high visibility vest for emergency responders, intended to draw attention away from the blood."

modular_nova/modules/loadouts/loadout_items/loadout_datum_suit.dm
@@ -120,6 +120,10 @@ GLOBAL_LIST_INIT(loadout_exosuits, generate_loadout_items(/datum/loadout_item/su
 	name = "Medical Labcoat"
 	item_path = /obj/item/clothing/suit/toggle/labcoat/medical
 
+/datum/loadout_item/suit/labcoat_regular
+	name = "Researcher's Labcoat"
+	item_path = /obj/item/clothing/suit/toggle/labcoat/nova/regular
+
 /*
 *	PONCHOS
 */

Skyrat-SS13/Skyrat-tg

modular_skyrat/master_files/code/modules/clothing/suits/labcoat.dm
@@ -28,11 +28,16 @@
 	icon_state = "labcoat_regular"
 	gets_cropped_on_taurs = FALSE
 
-/obj/item/clothing/suit/toggle/labcoat/nova/pharmacist
+/obj/item/clothing/suit/toggle/labcoat/skyrat/pharmacist
 	name = "pharmacist's labcoat"
 	desc = "A standard labcoat for chemistry which protects the wearer from acid spills."
 	icon_state = "labcoat_pharm"
 	gets_cropped_on_taurs = FALSE
+
+/obj/item/clothing/suit/toggle/labcoat/skyrat/regular
+	name = "researcher's labcoat"
+	desc = "A Nanotrasen standard labcoat for researchers in the scientific field."
+	icon_state = "labcoat_regular"
 
 /obj/item/clothing/suit/toggle/labcoat/skyrat/highvis
 	name = "high vis labcoat"

modular_skyrat/modules/loadouts/loadout_items/loadout_datum_suit.dm
@@ -126,7 +126,7 @@ GLOBAL_LIST_INIT(loadout_exosuits, generate_loadout_items(/datum/loadout_item/su
 
 /datum/loadout_item/suit/labcoat_pharmacist
 	name = "Pharmacist's Labcoat"
-	item_path = /obj/item/clothing/suit/toggle/labcoat/skyrat/pharmacist
+	item_path = /obj/item/clothing/suit/toggle/labcoat/skyrat/regular
 
 /*
 *	PONCHOS