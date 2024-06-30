AzriAnDr/HowlingVoid

[MIRROR] Moves "sprite accessories" (e.g. Hair, Undergarments, Mutant Bits) from `GLOB` to a datasystem (#2278)
* Moves "sprite accessories" (e.g. Hair, Undergarments, Mutant Bits) from `GLOB` to a datasystem

* Ugh

* .

* Continuing merging 82847 to Novas codebase (#3136)

done, i hope??

* Taur update

* lowercase a in accessories

---------

Co-authored-by: san7890 <the@san7890.com>
Co-authored-by: Mal <13398309+vinylspiders@users.noreply.github.com>
Co-authored-by: Iajret <8430839+Iajret@users.noreply.github.com>

code/__DEFINES/DNA.dm
@@ -61,7 +61,7 @@
 #define DNA_MONKEY_TAIL_BLOCK 16
 */ //NOVA EDIT REMOVAL END
 
-#define DNA_FEATURE_BLOCKS GLOB.dna_total_feature_blocks // NOVA EDIT CHANGE - ORIGINAL: #define DNA_FEATURE_BLOCKS 16
+#define DNA_FEATURE_BLOCKS SSaccessories.dna_total_feature_blocks // NOVA EDIT CHANGE - ORIGINAL: #define DNA_FEATURE_BLOCKS 16
 // NOVA EDIT ADDITION START
 #define DNA_MANDATORY_COLOR_BLOCKS 5
 #define DNA_MUTANT_COLOR_BLOCK 1

code/__HELPERS/dna.dm
@@ -15,4 +15,4 @@
 ///Getter macro used to get the length of a identity block
 #define GET_UI_BLOCK_LEN(blocknum) (GLOB.identity_block_lengths["[blocknum]"] || DNA_BLOCK_SIZE)
 ///Ditto, but for a feature.
-#define GET_UF_BLOCK_LEN(blocknum) (GLOB.features_block_lengths["[blocknum]"] || DNA_BLOCK_SIZE)
+#define GET_UF_BLOCK_LEN(blocknum) (SSaccessories.features_block_lengths["[blocknum]"] || DNA_BLOCK_SIZE) //NOVA CHANGE EDIT - Customization ORIGINAL: #define GET_UF_BLOCK_LEN(blocknum) (GLOB.features_block_lengths["[blocknum]"] || DNA_BLOCK_SIZE)

code/modules/food_and_drinks/restaurant/customers/_customer.dm
@@ -306,7 +306,7 @@
 /datum/customer_data/moth/proc/get_wings(mob/living/basic/robot_customer/customer)
 	var/customer_ref = WEAKREF(customer)
 	if (!LAZYACCESS(wings_chosen, customer_ref))
-		LAZYSET(wings_chosen, customer_ref, pick(GLOB.sprite_accessories["wings"]))
+		LAZYSET(wings_chosen, customer_ref, pick(SSaccessories.sprite_accessories["wings"])) // NOVA EDIT CHANGE - ORIGINAL: LAZYSET(wings_chosen, customer_ref, SSaccessories.moth_wings_list[pick(SSaccessories.moth_wings_list)])
 	return wings_chosen[customer_ref]
 
 /datum/customer_data/moth/get_underlays(mob/living/basic/robot_customer/customer)

code/modules/mining/lavaland/tendril_loot.dm
@@ -566,7 +566,7 @@
 	var/list/name2type = list()
 	for(var/obj/item/organ/external/wings/functional/possible_type as anything in wing_types)
 		var/datum/sprite_accessory/accessory = initial(possible_type.sprite_accessory_override) //get the type
-		accessory = GLOB.sprite_accessories[initial(accessory.key)][initial(accessory.name)] //NOVA EDIT CHANGE - ORIGINAL: accessory = GLOB.wings_list[initial(accessory.name)] //get the singleton instance
+		accessory = SSaccessories.sprite_accessories[initial(accessory.key)][initial(accessory.name)] //NOVA EDIT CHANGE - ORIGINAL: accessory = SSaccessories.wings_list[initial(accessory.name)] //get the singleton instance
 		var/image/img = image(icon = accessory.icon, icon_state = "m_wingsopen_[accessory.icon_state]_BEHIND") //Process the HUD elements
 		img.transform *= 0.5
 		img.pixel_x = -32

Skyrat-SS13/Skyrat-tg

code/__DEFINES/DNA.dm
@@ -65,7 +65,7 @@
 #define DNA_MONKEY_TAIL_BLOCK 16
 */ //SKYRAT EDIT REMOVAL END
 
-#define DNA_FEATURE_BLOCKS GLOB.dna_total_feature_blocks // SKYRAT EDIT CHANGE - ORIGINAL: #define DNA_FEATURE_BLOCKS 16
+#define DNA_FEATURE_BLOCKS SSaccessories.dna_total_feature_blocks // SKYRAT EDIT CHANGE - ORIGINAL: #define DNA_FEATURE_BLOCKS 16
 // SKYRAT EDIT ADDITION START
 #define DNA_MANDATORY_COLOR_BLOCKS 5
 #define DNA_MUTANT_COLOR_BLOCK 1

code/__HELPERS/dna.dm
@@ -15,4 +15,4 @@
 ///Getter macro used to get the length of a identity block
 #define GET_UI_BLOCK_LEN(blocknum) (GLOB.identity_block_lengths["[blocknum]"] || DNA_BLOCK_SIZE)
 ///Ditto, but for a feature.
-#define GET_UF_BLOCK_LEN(blocknum) (GLOB.features_block_lengths["[blocknum]"] || DNA_BLOCK_SIZE)
+#define GET_UF_BLOCK_LEN(blocknum) (SSaccessories.features_block_lengths["[blocknum]"] || DNA_BLOCK_SIZE) //SKYRAT CHANGE EDIT - Customization ORIGINAL: #define GET_UF_BLOCK_LEN(blocknum) (GLOB.features_block_lengths["[blocknum]"] || DNA_BLOCK_SIZE)

code/modules/food_and_drinks/restaurant/customers/_customer.dm
@@ -306,7 +306,7 @@
 /datum/customer_data/moth/proc/get_wings(mob/living/basic/robot_customer/customer)
 	var/customer_ref = WEAKREF(customer)
 	if (!LAZYACCESS(wings_chosen, customer_ref))
-		LAZYSET(wings_chosen, customer_ref, SSaccessories.moth_wings_list[pick(SSaccessories.moth_wings_list)])
+		LAZYSET(wings_chosen, customer_ref, pick(SSaccessories.sprite_accessories["wings"])) // SKYRAT EDIT CHANGE - ORIGINAL: LAZYSET(wings_chosen, customer_ref, SSaccessories.moth_wings_list[pick(SSaccessories.moth_wings_list)])
 	return wings_chosen[customer_ref]
 
 /datum/customer_data/moth/get_underlays(mob/living/basic/robot_customer/customer)

code/modules/mining/lavaland/tendril_loot.dm
@@ -566,7 +566,7 @@
 	var/list/name2type = list()
 	for(var/obj/item/organ/external/wings/functional/possible_type as anything in wing_types)
 		var/datum/sprite_accessory/accessory = initial(possible_type.sprite_accessory_override) //get the type
-		accessory = SSaccessories.sprite_accessories["wings"][initial(accessory.name)] //get the singleton instance // SKYRAT EDIT - Original SSaccessories.wings_list[initial(accessory.name)]
+		accessory = SSaccessories.sprite_accessories[initial(accessory.key)][initial(accessory.name)] //SKYRAT EDIT CHANGE - ORIGINAL: accessory = SSaccessories.wings_list[initial(accessory.name)] //get the singleton instance
 		var/image/img = image(icon = accessory.icon, icon_state = "m_wingsopen_[accessory.icon_state]_BEHIND") //Process the HUD elements
 		img.transform *= 0.5
 		img.pixel_x = -32