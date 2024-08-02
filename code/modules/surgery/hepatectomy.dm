AzriAnDr/HowlingVoid

Refactor Synths away from /Robot/ limbs, Cybernetically augmented humanoids have alternative surgeries (#3970)
* [NO SELF SURGERY] Cybernetically augmented humanoids have alternative surgeries (both standard and advanced) [NO SELF SURGERY], as well as other misc additions

* 1

* p1

* Update limbs.dm

* oh yeah Taurs

* Limbs and Techwebs for them

* 3

* EMP

* Surgery Removals from Synths

* 5

* test

* Look Ma, no more surgery

* Update tgstation.dme

* Update hepatectomy.dm

* little bit more edit cleanup and diff reset

* Update surgery.dm

* Synth Organ Surgery!

* Update lungs.dm

* target

* 4

* Update synth_defines.dm

* you add the metal before the zap zap

* Apply suggestions from code review

Co-authored-by: Bloop <13398309+vinylspiders@users.noreply.github.com>

* 1

* Update code/modules/surgery/hepatectomy.dm

* Update code/modules/surgery/advanced/brainwashing.dm

---------

Co-authored-by: necromanceranne <40847847+necromanceranne@users.noreply.github.com>
Co-authored-by: Fluffles <piecopresident@gmail.com>
Co-authored-by: Bloop <13398309+vinylspiders@users.noreply.github.com>

code/modules/surgery/hepatectomy.dm
@@ -13,6 +13,18 @@
 		/datum/surgery_step/close,
 	)
 
+/datum/surgery/hepatectomy/mechanic
+	name = "Impurity Management System Diagnostic"
+	requires_bodypart_type = BODYTYPE_ROBOTIC
+	steps = list(
+		/datum/surgery_step/mechanic_open,
+		/datum/surgery_step/open_hatch,
+		/datum/surgery_step/mechanic_unwrench,
+		/datum/surgery_step/hepatectomy/mechanic,
+		/datum/surgery_step/mechanic_wrench,
+		/datum/surgery_step/mechanic_close,
+	)
+
 /datum/surgery/hepatectomy/can_start(mob/user, mob/living/carbon/target)
 	var/obj/item/organ/internal/liver/target_liver = target.get_organ_slot(ORGAN_SLOT_LIVER)
 	if(isnull(target_liver) || target_liver.damage < 50 || target_liver.operated)
@@ -34,6 +46,17 @@
 	failure_sound = 'sound/surgery/organ2.ogg'
 	surgery_effects_mood = TRUE
 
+/datum/surgery_step/hepatectomy/mechanic
+	name = "perform maintenance (scalpel or wirecutter)"
+	implements = list(
+		TOOL_SCALPEL = 95,
+		TOOL_WRENCH = 95,
+		/obj/item/melee/energy/sword = 65,
+		/obj/item/knife = 45,
+		/obj/item/shard = 35)
+	preop_sound = 'sound/items/ratchet.ogg'
+	success_sound = 'sound/machines/doorclick.ogg'
+
 /datum/surgery_step/hepatectomy/preop(mob/user, mob/living/carbon/target, target_zone, obj/item/tool, datum/surgery/surgery)
 	display_results(
 		user,
@@ -48,13 +71,15 @@
 	var/mob/living/carbon/human/human_target = target
 	var/obj/item/organ/internal/liver/target_liver = target.get_organ_slot(ORGAN_SLOT_LIVER)
 	human_target.setOrganLoss(ORGAN_SLOT_LIVER, 10) //not bad, not great
-	//NOVA EDIT ADDITION BEGIN - This is so that you can do organ surgeries multiple times on slimepeople.
 	if(target_liver)
+		//NOVA EDIT ADDITION BEGIN - This is so that you can do organ surgeries multiple times on slimepeople.
 		if(istype(target_liver, /obj/item/organ/internal/liver/slime))
 			addtimer(VARSET_CALLBACK(target_liver, operated, FALSE), 30 SECONDS)
 		else
-			target_liver.operated = TRUE
-	//NOVA EDIT ADDITION END
+		//NOVA EDIT ADDITION END
+			target_liver.operated = TRUE // NOVA EDIT CHANGE - Not for slimepeople - Indents for else block
+		if(target_liver.organ_flags & ORGAN_EMP) //If our organ is failing due to an EMP, fix that
+			target_liver.organ_flags &= ~ORGAN_EMP
 	display_results(
 		user,
 		target,

Skyrat-SS13/Skyrat-tg

code/modules/surgery/hepatectomy.dm
@@ -13,6 +13,18 @@
 		/datum/surgery_step/close,
 	)
 
+/datum/surgery/hepatectomy/mechanic
+	name = "Impurity Management System Diagnostic"
+	requires_bodypart_type = BODYTYPE_ROBOTIC
+	steps = list(
+		/datum/surgery_step/mechanic_open,
+		/datum/surgery_step/open_hatch,
+		/datum/surgery_step/mechanic_unwrench,
+		/datum/surgery_step/hepatectomy/mechanic,
+		/datum/surgery_step/mechanic_wrench,
+		/datum/surgery_step/mechanic_close,
+	)
+
 /datum/surgery/hepatectomy/can_start(mob/user, mob/living/carbon/target)
 	var/obj/item/organ/internal/liver/target_liver = target.get_organ_slot(ORGAN_SLOT_LIVER)
 	if(isnull(target_liver) || target_liver.damage < 50 || target_liver.operated)
@@ -34,6 +46,17 @@
 	failure_sound = 'sound/surgery/organ2.ogg'
 	surgery_effects_mood = TRUE
 
+/datum/surgery_step/hepatectomy/mechanic
+	name = "perform maintenance (scalpel or wirecutter)"
+	implements = list(
+		TOOL_SCALPEL = 95,
+		TOOL_WRENCH = 95,
+		/obj/item/melee/energy/sword = 65,
+		/obj/item/knife = 45,
+		/obj/item/shard = 35)
+	preop_sound = 'sound/items/ratchet.ogg'
+	success_sound = 'sound/machines/doorclick.ogg'
+
 /datum/surgery_step/hepatectomy/preop(mob/user, mob/living/carbon/target, target_zone, obj/item/tool, datum/surgery/surgery)
 	display_results(
 		user,
@@ -49,7 +72,14 @@
 	var/obj/item/organ/internal/liver/target_liver = target.get_organ_slot(ORGAN_SLOT_LIVER)
 	human_target.setOrganLoss(ORGAN_SLOT_LIVER, 10) //not bad, not great
 	if(target_liver)
-		target_liver.operated = TRUE
+		//SKYRAT EDIT ADDITION BEGIN - This is so that you can do organ surgeries multiple times on slimepeople.
+		if(istype(target_liver, /obj/item/organ/internal/liver/slime))
+			addtimer(VARSET_CALLBACK(target_liver, operated, FALSE), 30 SECONDS)
+		else
+		//SKYRAT EDIT ADDITION END
+			target_liver.operated = TRUE // SKYRAT EDIT CHANGE - Not for slimepeople - Indents for else block
+		if(target_liver.organ_flags & ORGAN_EMP) //If our organ is failing due to an EMP, fix that
+			target_liver.organ_flags &= ~ORGAN_EMP
 	display_results(
 		user,
 		target,