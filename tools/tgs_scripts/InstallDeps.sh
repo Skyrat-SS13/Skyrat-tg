86a5264e17b70d2e3be6e0a82295f93bb30a22b4
Lucy <lucy@absolucy.moe>
2024-08-19 07:09:40 -0400

    Use yt-dlp by default instead of youtube-dl

--- tools/tgs_scripts/InstallDeps.sh
+++ tools/tgs_scripts/InstallDeps.sh
@@ -6,7 +6,7 @@ has_git="$(command -v git)"
 has_curl="$(command -v curl)"
 has_cargo="$(command -v ~/.cargo/bin/cargo)"
 has_sudo="$(command -v sudo)"
-has_youtubedl="$(command -v youtube-dl)"
+has_ytdlp="$(command -v yt-dlp)"
 has_pip3="$(command -v pip3)"
 set -e
 set -x
@@ -32,8 +32,9 @@ if ! [ -x "$has_cargo" ]; then
 	. ~/.profile
 fi
 
-# install or update youtube-dl when not present, or if it is present with pip3,
+# install or update yt-dlp when not present, or if it is present with pip3,
 # which we assume was used to install it
+<<<<<<< HEAD
 if ! [ -x "$has_youtubedl" ]; then
 	echo "Installing youtube-dl with pip3..."
 	if ! [ -x "$has_sudo" ]; then
@@ -47,4 +48,12 @@ if ! [ -x "$has_youtubedl" ]; then
 elif [ -x "$has_pip3" ]; then
 	echo "Ensuring youtube-dl is up-to-date with pip3..."
 	pip3 install youtube-dl -U --break-system-packages
+=======
+if ! [ -x "$has_ytdlp" ]; then
+	echo "Installing yt-dlp with pip3..."
+	pip3 install yt-dlp --break-system-packages
+else
+	echo "Ensuring yt-dlp is up-to-date with pip3..."
+	pip3 install yt-dlp -U --break-system-packages
+>>>>>>> 368ca25e6b64 (Use yt-dlp by default instead of youtube-dl (#85953))
 fi
