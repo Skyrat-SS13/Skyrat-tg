8565d28d10f09725a9a1b14450d20155e9d1c6d7
projectkepler-RU <99981766+projectkepler-ru@users.noreply.github.com>
2024-08-21 00:05:37 +0000

    done

--- tools/tgs_scripts/InstallDeps.sh
+++ tools/tgs_scripts/InstallDeps.sh
@@ -34,26 +34,10 @@ fi
 
 # install or update yt-dlp when not present, or if it is present with pip3,
 # which we assume was used to install it
-<<<<<<< HEAD
-if ! [ -x "$has_youtubedl" ]; then
-	echo "Installing youtube-dl with pip3..."
-	if ! [ -x "$has_sudo" ]; then
-		apt-get update
-		apt-get install -y python3 python3-pip
-	else
-		sudo apt-get update
-		sudo apt-get install -y python3 python3-pip
-	fi
-	pip3 install youtube-dl --break-system-packages
-elif [ -x "$has_pip3" ]; then
-	echo "Ensuring youtube-dl is up-to-date with pip3..."
-	pip3 install youtube-dl -U --break-system-packages
-=======
 if ! [ -x "$has_ytdlp" ]; then
 	echo "Installing yt-dlp with pip3..."
 	pip3 install yt-dlp --break-system-packages
 else
 	echo "Ensuring yt-dlp is up-to-date with pip3..."
 	pip3 install yt-dlp -U --break-system-packages
->>>>>>> 368ca25e6b64 (Use yt-dlp by default instead of youtube-dl (#85953))
 fi
